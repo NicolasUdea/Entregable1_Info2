2fbba216d5b7f61960589f1e3b6c7640f710e5d9
juangarcesr <juan.garcesr@udea.edu.co>
2023-09-16 13:17:47 -0500

    codigo completo

--- clases_nuevas.py
+++ clases_nuevas.py
@@ -5,7 +5,7 @@ class Maquina:
         self.__modelo = 0
         self.__empresa = ""
     
-    @property  
+    @property 
     def precio(self):
         return self.__precio
     @precio.setter
@@ -97,3 +97,31 @@ class BaseDatos(Maquina):
     
     def agregarMaquina(self, Maquina):
         self.__inventario[Maquina.verModelo()] = Maquina
+def main():
+
+    b=Maquina()
+    while True:
+        print("Seleccione una opción: \n1.inresar una nueva máquina. \n 2.Eliminar una máquina \n 3.Selección de una máquina. \n 4.Editar datos de máquina \n 5.Salir")
+        a=input("ingrese un opción: ")
+        if a == "1": 
+            b=BaseDatos
+            b.precio=(float(input("ingrese el precio de la máquina: ")))
+            b.stock=(float(input("ingrese el stock: ")))
+            b.empresa=(input("ingrese la empresa: "))
+            b.modelo=(float(input("ingrese el modelo de la maquina: ")))
+            
+            c=input("ingrese el tipo de máqiuina que desea ingresar: \n 1 máquina para electrocardiografía \n 2 Desfibrilador hospitalario \n 3 máquina para resonancia magnética ")
+            if c == "1":
+                b.derivaciones=(float(input("ingrese el número de derivaciones: ")))
+
+            elif c == "2":
+                b.energia=(float(input("ingrese la energía máxima del desfibrilador: ")))
+
+            else:
+                b.intensidad=(float(input("ingrese la intensidad del campo magnético en teslas: ")))
+        
+        b.agrearMaquina(b)
+
+
+if __name__ == '__main__':
+    main()
